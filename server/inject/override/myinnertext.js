8446fdbaa6d6cf8b19df44076d3ae82283a76f6b
Martha Jimenez <martha.jimenez@flex.com>
2017-09-07 16:52:24 -0500

    added myInnterTest

---
+++ server/inject/override/myinnertext.js
@@ -0,0 +1,25 @@
+powwow.controls.myInnerText = {
+    set: function (scopedoc, input, value, events) {
+        return new Promise(function (resolve) {
+            var newValue = (value === undefined) ? '' : value;
+            if (input.innerText !== newValue) {
+                input.innerText = newValue;
+                if (!events) {
+                    events = "keyupEvent";
+                }
+                powwow._fireEvents(input, scopedoc, events, resolve);
+            } else if (events) {
+                powwow._fireEvents(input, scopedoc, events, resolve);
+            } else {
+                resolve();
+            }
+        });
+    },
+    /*
+     * Get a property of a node.  Node itself is returned if there is no property.
+     */
+    get: function (node) {
+        return node['innerText'];
+    }
+}
+
