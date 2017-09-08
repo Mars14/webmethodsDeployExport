23994c5092a82a3761a3bec900bc5e6bdc3148c7
Martha Jimenez <martha.jimenez@flex.com>
2017-09-08 16:09:05 -0500

    version 1

--- server/api/projectlist.js
+++ server/api/projectlist.js
@@ -3,5 +3,12 @@ var config = require('../config');
 var browser =  require('powwow-server-common').browser;
 
 exports.updatesearch = function (page, params) {
+    //delete params.listItems;
     page.update('flexdeployerprojects', params);
+
+
+    setTimeout(function() {
+        page.extract('flexdeployerprojects')
+            .screen('flexdeployerprojects');
+    }, 1000);
 }
